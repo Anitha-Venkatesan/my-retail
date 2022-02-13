192a86a18de09d64b8b14a6986e467963f59a5fb
Anitha Venkatesan <anithamca68@gmail.com>
2022-02-12 20:11:39 -0600

    added exception

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Price } from './price';
 import { Product } from './product';
 import axios from 'axios';
@@ -6,17 +6,27 @@ import axios from 'axios';
 @Injectable()
 export class ProductService {
   async getProductsId(id: number): Promise<Product> {
-    const response = await axios.get(
-      `https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=${id}`,
-    );
+    const title = await this.getProductNameByID(id);
     const product = new Product();
-    const productName =
-      response.data.data.product.item.product_description.title;
     product.id = id;
-    product.name = productName;
+    product.name = title;
     return product;
   }
   editProductsId(id: number, price: Price): Price {
     return price;
+  }
+  async getProductNameByID(id: number): Promise<string> {
+    let response;
+    try {
+      response = await axios.get(
+        `https://redsky-uat.perf.target.com/redsky_aggregations/v1/redsky/case_study_v1?key=3yUxt7WltYG7MFKPp7uyELi1K40ad2ys&tcin=${id}`,
+      );
+    } catch (error) {
+      const statusCode = error.response.status;
+      const message = error.response.statusText;
+      throw new HttpException(message, statusCode);
+    }
+    const title = response.data.data.product.item.product_description.title;
+    return title;
   }
 }
