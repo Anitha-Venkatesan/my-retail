b0d4cd60f73d78b27c5f873cb913dd7dce8519fc
Anitha Venkatesan <anithamca68@gmail.com>
2022-02-12 20:50:55 -0600

    added swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  getSwaggerModule(app);
   await app.listen(3000);
 }
+function getSwaggerModule(app) {
+  const config = new DocumentBuilder()
+    .setTitle('myRetail')
+    .setDescription('API for myRetail application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
 bootstrap();
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -9,15 +9,38 @@ import {
 import { ProductService } from './product.service';
 import { Price } from './price';
 import { Product } from './product';
+import { ApiNotFoundResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@Controller()
+@Controller('/products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
-  @Get('/products/:id')
+  @Get('/:id')
+  @ApiOperation({
+    summary: 'Get Products by Id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product information',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Product not found',
+  })
   async getProductsId(@Param('id', ParseIntPipe) id: number): Promise<Product> {
     return this.productService.getProductsId(id);
   }
-  @Put('/products/:id')
+  @Put('/:id')
+  @ApiOperation({
+    summary: 'Update Products by Id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Product updated successfully',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Product not found',
+  })
   editProductsId(
     @Param('id', ParseIntPipe) id: number,
     @Body() price: Price,
